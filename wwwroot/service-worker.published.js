9ac1be343c2585b108062cc3dbd20514b1300771
Tobotobo <tobotobo.channel@gmail.com>
2024-05-28 22:52:28 +0900

    テスト

--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -1,5 +1,7 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
+// 注意！アプリケーションを公開する前に、注意事項を必ず理解してください。
+// オフラインサポート。 https://aka.ms/blazor-offline-considerations を参照してください。
 
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
@@ -8,11 +10,12 @@ self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
-const offlineAssetsInclude = [/\.br$/, /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/];
+const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/];
 const offlineAssetsExclude = [/^service-worker\.js$/];
 
 // Replace with your base path if you are hosting on a subfolder. Ensure there is a trailing '/'.
-const base = "/";
+// サブフォルダーでホストしている場合は、ベース パスに置き換えます。末尾に「/」があることを確認してください。
+const base = "/dotnet_blazor_webassembly_example1/";
 const baseUrl = new URL(base, self.origin);
 const manifestUrlList = self.assetsManifest.assets.map(asset => new URL(asset.url, baseUrl).href);
 
@@ -21,6 +24,7 @@ async function onInstall(event) {
     self.skipWaiting();
 
     // Fetch and cache all matching items from the assets manifest
+    // アセットマニフェストから一致するすべてのアイテムを取得してキャッシュします
     const assetsRequests = self.assetsManifest.assets
         .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
         .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
@@ -32,6 +36,7 @@ async function onActivate(event) {
     console.info('Service worker: Activate');
 
     // Delete unused caches
+    // 使用されていないキャッシュを削除する
     const cacheKeys = await caches.keys();
     await Promise.all(cacheKeys
         .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
@@ -44,6 +49,9 @@ async function onFetch(event) {
         // For all navigation requests, try to serve index.html from cache,
         // unless that request is for an offline resource.
         // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
+        // すべてのナビゲーション リクエストに対して、キャッシュからのindex.htmlの提供を試みます。
+        // ただし、そのリクエストがオフライン リソースに対するものである場合は除きます。
+        // いくつかの URL をサーバーでレンダリングする必要がある場合は、次のチェックを編集してそれらの URL を除外します
         const shouldServeIndexHtml = event.request.mode === 'navigate'
             && !manifestUrlList.some(url => url === event.request.url);
 
